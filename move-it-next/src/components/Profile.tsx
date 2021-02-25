74480b5013254fb9b1de41704cd92adf6c932776
fabio-peres <fabioperesg@gmail.com>
2021-02-25 11:00:09 -0300

    Criação componente Profile

---
+++ move-it-next/src/components/Profile.tsx
@@ -0,0 +1,12 @@
+export function Profile() {
+  return (
+    <div>
+      <img src="https://github.com/fabio-peres.png" alt="Fabio Peres"/>
+      <div>
+        <strong>Fabio Peres</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+
+  )
+}
